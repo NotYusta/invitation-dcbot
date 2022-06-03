65f1ac9ffcc5b1b97341f0855973b8944082f1ce
Yusta <xabhista19@gmail.com>
2022-06-03 12:02:22 +0700

    feat: add config

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
 import { configSection } from './utils/config.js';
+import { Client } from 'discord.js';
 
+const client = new Client({
+    intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS', 'GUILD_MESSAGE_TYPING', 'DIRECT_MESSAGES', 'DIRECT_MESSAGE_REACTIONS', 'DIRECT_MESSAGE_TYPING']
+});
+ 
+console.log("Logging in...");
+client.login(configSection.bot.token).then(() => {
+    if(client.user == null) return;
+    console.log("Logged in as " + client.user.tag);
+});
